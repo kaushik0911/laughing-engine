d61c5366c516acbd24f9581fe96bd23cb480b2a8
kaushik911 <kaushikshamantha@gmail.com>
2024-12-09 02:03:45 +0400

    * Remove unwanted import

--- task_2_data_analysis/most_scoring_quarter/src/main/java/org/iit/kaushik20241270/MostScoringQuarterMapper.java
+++ task_2_data_analysis/most_scoring_quarter/src/main/java/org/iit/kaushik20241270/MostScoringQuarterMapper.java
@@ -1,6 +1,5 @@
 package org.iit.kaushik20241270;
 
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
